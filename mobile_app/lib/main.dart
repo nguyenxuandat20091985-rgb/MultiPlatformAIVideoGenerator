dc2dbfea852657d6320017555446d40c02fd252d
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-19 04:19:12 +0700

    Home screen: API URL field + SharedPreferences; bump app to 1.0.1+2

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
-import 'services/api_service.dart';
+
 import 'screens/home_screen.dart';
+import 'services/api_service.dart';
+import 'services/settings_service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MultiPlatformVideoApp());
 }
 
-class MultiPlatformVideoApp extends StatelessWidget {
+class MultiPlatformVideoApp extends StatefulWidget {
   const MultiPlatformVideoApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final api = ApiService();
+  State<MultiPlatformVideoApp> createState() => _MultiPlatformVideoAppState();
+}
 
+class _MultiPlatformVideoAppState extends State<MultiPlatformVideoApp> {
+  ApiService? _api;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _bootstrap();
+  }
+
+  Future<void> _bootstrap() async {
+    final url = await SettingsService.instance.getApiBaseUrl();
+    if (!mounted) return;
+    setState(() {
+      _api = ApiService(baseUrl: url);
+      _loading = false;
+    });
+  }
+
+  void _onApiUrlChanged(String url) {
+    _api?.setBaseUrl(url);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AI Video Generator',
       debugShowCheckedModeBanner: false,
@@ -23,9 +51,7 @@ class MultiPlatformVideoApp extends StatelessWidget {
           brightness: Brightness.light,
         ),
         useMaterial3: true,
-        inputDecorationTheme: const InputDecorationTheme(
-          filled: true,
-        ),
+        inputDecorationTheme: const InputDecorationTheme(filled: true),
       ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
@@ -34,7 +60,14 @@ class MultiPlatformVideoApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: HomeScreen(api: api),
+      home: _loading || _api == null
+          ? const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            )
+          : HomeScreen(
+              api: _api!,
+              onApiUrlChanged: _onApiUrlChanged,
+            ),
     );
   }
 }
